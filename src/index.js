9931513fe0ed20abe530d5c178ca853d30770cb3
Sjoshi7 <sjoshise7@gmail.com>
2019-11-13 08:05:57 +0530

    questions methods

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import React, {Component} from 'react';
 import "./style.css";
+import ReactDOM from 'react-dom';
+import quizService from './quizService';
+
+class Quiz extends Component {
+  state = {
+    questionBank:[]
+  };
+  getQuestion = () => {
+    quizService().then(question=> {
+      this.setState({
+        questionBank:question
+      });
+    });
+  };
+  componentDidMount(){
+    this.getQuestion();
+  }
+  render(){
+    return(
+      <div className="container">
+        <div className="title">QuizBee</div>
+        {this.state.questionBank.length > 0 && this.state.questionBank.map(
+          ({question,answers,correct,questionId})=><h4>{question}</h4>
+        )}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Quiz/>,document.getElementById('root'));
